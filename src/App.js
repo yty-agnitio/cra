3159a743889377316de6a1ac5018bfb55a585d79
yty-agnitio <7777kara@gmail.com>
2020-11-25 18:31:22 +0100

    adds text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         test codeship
+         test AWS Code Pipeline!
         </p>
         <a
           className="App-link"
