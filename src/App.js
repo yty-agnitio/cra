f04592f7e4fd7afe3f93dc0318e5460561979d4b
yty-agnitio <7777kara@gmail.com>
2020-11-26 09:36:15 +0100

    editing text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         test AWS Code Pipeline!
+         AWS Code Pipeline test
         </p>
         <a
           className="App-link"
